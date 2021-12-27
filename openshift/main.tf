3e4ea93b6613a269e47bef4d427742b98a271642
Mayra Espinoza Reyes <mayraer11@gmail.com>
2021-12-27 00:19:18 -0500

    correción rg

--- openshift/main.tf
+++ openshift/main.tf
@@ -4,7 +4,7 @@ resource "ibm_resource_group" "rg" {
 }
 
 data "ibm_resource_group" "resourceGroup" {
-  name = var.resource_group_name != " " ? var.resource_group_name : "rg-${var.project}-${var.environment}"
+  name = var.resource_group_name != " " ? var.resource_group_name : ibm_resource_group.rg.name
 }
 
 resource "ibm_resource_instance" "cos_instance" {
