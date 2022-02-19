d0fd4cb3bb880997544e0f218f089260afb4684c
Mayra Espinoza Reyes <mayraer11@gmail.com>
2022-02-18 21:05:21 -0500

    Test

--- VM/main.tf
+++ VM/main.tf
@@ -11,6 +11,7 @@ data ibm_is_image "image_vm" {
   name = var.image
 }
 
+/*
 resource ibm_is_instance "vm" {
   name    = "vm-${var.project}-${var.environment}-001"
   resource_group = data.ibm_resource_group.resourceGroup.id
@@ -31,6 +32,6 @@ resource ibm_is_instance "vm" {
     allow_ip_spoofing = false
   }
 }
-
+*/
 
 
