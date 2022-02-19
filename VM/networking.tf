ee07e33808f4839b0fe20c6bb5675d7524586309
Mayra Espinoza Reyes <mayraer11@gmail.com>
2022-02-19 00:02:17 -0500

    Retiro de comentarios

--- VM/networking.tf
+++ VM/networking.tf
@@ -12,25 +12,6 @@ resource ibm_is_subnet "vpc_subnet" {
   ipv4_cidr_block = var.cdir
 }
 
-/*
-resource ibm_is_security_group "vpc_security_group" {
-  name = "sg-${var.project}-${var.environment}-001"
-  vpc  = ibm_is_vpc.vpc_vm.id
-}
-*/
-
-resource "ibm_is_security_group_rule" "ingress_ssh_all" {
-#  group     = ibm_is_security_group.vpc_security_group.id
-  group     = ibm_is_vpc.security_group.id
-  direction = "inbound"
-  remote    = "0.0.0.0/0"
-
-  tcp {
-    port_min = 22
-    port_max = 22
-  }
-}
-
 resource ibm_is_floating_ip "vpc_fip" {
   name   = "fip-${var.project}-${var.environment}-001"
   target = ibm_is_instance.vpc_vsi.primary_network_interface.0.id
